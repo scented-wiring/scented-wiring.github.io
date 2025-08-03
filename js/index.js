d1ba321ff643c3954bc632dc9298610496817983
scented-wiring <66779908+scented-wiring@users.noreply.github.com>
2025-08-03 11:16:55 +0100

    Format code, rm swiper

--- js/index.js
+++ js/index.js
@@ -92,14 +92,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
-
-  const gallerySlider = new Swiper(".gallery-swiper.swiper", {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    speed: 2000,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    }
-  });
